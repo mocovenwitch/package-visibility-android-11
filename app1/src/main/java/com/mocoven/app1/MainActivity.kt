85371a0c5b99ce4c583dcbaf7c835645d4f7fa60
mocovenwitch <mocovenwitch@gmail.com>
2020-09-25 19:34:33 +1000

    tag1 app1 app2 app3

---
+++ app1/src/main/java/com/mocoven/app1/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.mocoven.app1
+
+import android.net.Uri
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, getDataFromApp2ContentProvider(), Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun getDataFromApp2ContentProvider(): String {
+        var result = "null"
+
+        // fetch 2
+        var cursor = this.contentResolver.query(
+            Uri.parse("content://com.mocoven.app2.provider/name"),
+            null,
+            null,
+            null,
+            null
+        )
+        cursor?.let {
+            if (it.count > 0) {
+                it.moveToFirst()
+                result = it.getString(0)
+            }
+        }
+
+        // fetch3
+        cursor = this.contentResolver.query(
+            Uri.parse("content://com.mocoven.app3.provider/name"),
+            null,
+            null,
+            null,
+            null
+        )
+        cursor?.let {
+            if (it.count > 0) {
+                it.moveToFirst()
+                result += it.getString(0)
+            }
+        }
+
+        cursor?.close()
+        return result
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
